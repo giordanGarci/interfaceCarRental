edc275a504f06e377d25064163cbc4b98e1604c3
Giordan Garcia <139655484+giordanGarci@users.noreply.github.com>
2023-07-31 11:43:27 -0300

    Update BrazilTaxService.java

--- src/model/services/BrazilTaxService.java
+++ src/model/services/BrazilTaxService.java
@@ -2,7 +2,7 @@ package model.services;
 
 public class BrazilTaxService implements TaxService {
 
-	@Override
+
 	public Double tax(Double amount) {
 		double taxMultiplier = (amount <= 100) ? 0.2 : 0.15;
 		return amount * taxMultiplier;
